007c0b586c06107f83a04d9522d45231e43cc49c
xenoxndr <156947918+xenoxndr@users.noreply.github.com>
2025-12-09 16:06:38 -0600

    feat:read and write

---
+++ sigmafier.py
@@ -0,0 +1,21 @@
+import discord
+
+class SigmaClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+    async def on_message(self, message):
+        # we do not want the bot to reply to itself
+        if message.author.id == self.user.id:
+            return
+
+        if message.content.startswith('!hello motto'):
+            await message.reply('ur actually rlly cool, and you dont smell like pizza', mention_author=True)
+        elif message.content.startswith('!hello'):
+            await message.reply('still stinky', mention_author=True)
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = SigmaClient(intents=intents)
+client.run()
