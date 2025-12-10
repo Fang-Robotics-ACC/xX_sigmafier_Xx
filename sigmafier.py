6d9f3e33a3f8fe6480bdbddaafdc3cafc7d13570
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-12-10 16:33:48 -0600

    feat: Enable accessing member lists

--- sigmafier.py
+++ sigmafier.py
@@ -27,6 +27,7 @@ class SigmaClient(discord.Client):
 
 intents = discord.Intents.default()
 intents.message_content = True
+intents.members = True
 
 client = SigmaClient(intents=intents)
 client.run(token)
