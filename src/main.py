90bd34ca066947c25ac22c0d3a70b49166d8be3d
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-12-16 10:54:22 -0600

    feat: Have main entry point for program

---
+++ src/main.py
@@ -0,0 +1,10 @@
+def main():
+    # main
+# For the uninitiated, this a main guard
+# It will only execute main if this file is the "entry point"
+# If this file happens to be imported for some reason, main() will
+# not be automatically executed
+# https://stackoverflow.com/questions/419163/what-does-if-name-main-do#419185
+
+if __name__ == "__main__":
+    main()
