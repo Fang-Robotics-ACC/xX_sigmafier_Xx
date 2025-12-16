c20bccd564fd647528f841129784bc23ab90fbde
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-12-16 11:02:47 -0600

    refactor: Have main function programming in main :P

--- src/main.py
+++ src/main.py
@@ -1,5 +1,19 @@
+from os import environ
+from dotenv import load_dotenv 
+
+
+
 def main():
-    # main
+
+    load_dotenv()
+    token = environ['TOKEN']
+
+    intents = discord.Intents.default()
+    intents.message_content = True
+    intents.members = True
+
+    client = SigmaClient(intents=intents)
+    client.run(token)
 # For the uninitiated, this a main guard
 # It will only execute main if this file is the "entry point"
 # If this file happens to be imported for some reason, main() will
